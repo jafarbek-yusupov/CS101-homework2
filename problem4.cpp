7ad7b9c55844912b6400a51b73e6eff8c85735be
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-09-16 14:45:13 +0500

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n1, n2, n3, n4;
+  int 
+  cin >> n1 >> n2 >> n3 >> n4;
+
+  //case 1 - 4 equal
+  //if(n1 == n2 || n2 == n3 || n3 == n4 || n1==n3 || n1==n4)  cout << 
+  //case2 - 3 equal
+  //case 3.1 - 2*2 equal
+  //case 3.2 - 2 equal, 2diff
+  //case 4 - all diff
+  
+  
+  return 0;
+}
