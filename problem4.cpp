36b5a7ac7645bb56d511589734b017fd6c431455
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-09-16 16:05:20 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -2,17 +2,32 @@
 using namespace std;
 
 int main() {
-  int n1, n2, n3, n4;
-  int 
-  cin >> n1 >> n2 >> n3 >> n4;
+    int n1, n2, n3, n4;
+    cin >> n1 >> n2 >> n3 >> n4;
+    int ans = 0;
+    int sims = 0;
 
-  //case 1 - 4 equal
-  //if(n1 == n2 || n2 == n3 || n3 == n4 || n1==n3 || n1==n4)  cout << 
-  //case2 - 3 equal
-  //case 3.1 - 2*2 equal
-  //case 3.2 - 2 equal, 2diff
-  //case 4 - all diff
+    if (n1 == n2) sims++;
+    if (n1 == n3) sims++;
+    if (n1 == n4) sims++;
+    if (n2 == n3) sims++;
+    if (n2 == n4) sims++;
+    if (n3 == n4) sims++;
+    
+    cout << sims << endl;
+    switch(sims){
+      case(6):  
+        ans=3;
+        break;
+      case(3):  
+        ans=2;
+        break;      
+      default:
+        ans = sims
+        break;
+    }
   
-  
-  return 0;
+    cout << ans << endl;
+
+    return 0;
 }
